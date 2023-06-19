7ee88e5d0d01088d8255b658870f3c191a51ebd3
Michael Walters <Michael1996Walters@gmail.com>
2023-06-19 17:33:19 +0100

    testing db storage4

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,11 +13,11 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (value) => {
+export const putDb = async (content) => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ value });
+  const request = store.put({ value: content });
   const result = await request;
   console.log("Data saved to the database check", result);
 };
