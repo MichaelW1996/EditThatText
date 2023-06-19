4a91385d242b4efb18f39f19f7d8aaa853bdd5d5
Michael Walters <Michael1996Walters@gmail.com>
2023-06-19 17:02:28 +0100

    Bug fix attempt 3

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,9 +1,5 @@
 import { openDB } from "idb";
 
-(async () => {
-  await initdb();
-})();
-
 const initdb = async () =>
   openDB("jate", 1, {
     upgrade(db) {
@@ -17,11 +13,11 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async (id, value) => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ jate: content });
+  const request = store.put({ id, value });
   const result = await request;
   console.log("Data saved to the database", result);
 };
@@ -36,3 +32,5 @@ export const getDb = async () => {
   console.log("Retreived data from database", result);
   return result;
 };
+
+initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,20 +4,26 @@ const butInstall = document.getElementById("buttonInstall");
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
   event.preventDefault();
+  window.deferredPrompt = event;
   butInstall.style.visibility = "visible";
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
   const promptEvent = window.deferredPrompt;
-  if (promptEvent) {
-    promptEvent.prompt();
+  if (!promptEvent) {
+    return;
   }
-  butInstall.setAttribute("disabled", true);
-  butInstall.textContent = "Installed!";
+  // prompt user
+  promptEvent.prompt();
+  // Reset the deferred prompt
+  window.deferredPrompt = null;
+  console.log("click");
+  butInstall.classList.toggle("hidden", true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
-  console.log("JATE app installed", event); //maybe add an alert?
+  console.log("JATE app installed", event);
+  window.deferredPrompt = null;
 });
