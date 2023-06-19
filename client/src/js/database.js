018a1dfc2dbc8ce6daf769f0590d6c7ff4e615df
Michael Walters <Michael1996Walters@gmail.com>
2023-06-19 17:47:00 +0100

    testing db storage5

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,12 +14,16 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const jateDb = await openDB("jate", 1);
-  const tx = jateDb.transaction("jate", "readwrite");
-  const store = tx.objectStore("jate");
-  const request = store.put({ value: content });
-  const result = await request;
-  console.log("Data saved to the database check", result);
+  if (content != null) {
+    const jateDb = await openDB("jate", 1);
+    const tx = jateDb.transaction("jate", "readwrite");
+    const store = tx.objectStore("jate");
+    const request = store.put({ value: content });
+    const result = await request;
+    console.log("Data saved to the database check", result);
+  } else {
+    console.log("Tried to give null value");
+  }
 };
 
 // TODO: Add logic for a method that gets all the content from the database
